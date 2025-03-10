5dc56fbcc84a09df8a57119600690b3f8e6bc9cc
Tom Woodling <t.woodling@coolblue.nl>
2025-03-10 13:49:17 +0100

    further changes

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,7 +1,5 @@
 extends Node3D
 
 func _ready() -> void:
-	# After world setup is complete, notify GameManager
-	var player = get_tree().get_first_node_in_group("player")
-	if player:
-		GameManager.world_ready(player)
+	# no longer needed
+	pass
